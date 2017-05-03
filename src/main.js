2cbad6bd92378f439a5dcebaaf582213d27c8a41
cyberblast <git@cyberblast.org>
2017-05-03 09:53:08 +0200

    Delete main.js

--- src/main.js
+++
@@ -1,6 +0,0 @@
-const engine = require('engine.system');
-module.exports.loop = function () {
-    // removing a feature will also clear its memory
-    engine.registerFeature("sample");
-    engine.run();
-};
