cddff8110215f8943df4944fe9f58da3b233d41a
cyberblast <git@cyberblast.org>
2017-05-06 11:30:52 +0200

    some changes

--- src/example.main.js
+++ src/example.main.js
@@ -4,6 +4,9 @@ const engine = require('engine.system');
 module.exports.loop = function () {
     // set your ingame username. may be used in some features...
     global.USERNAME = 'cyberblast';
+    // friendly players which should not get attacked
+    global.PLAYER_WHITELIST = [];
+    
     // removing (not registering) a feature will also clear its memory
     // add more features here...
     engine.registerFeature("sample");
