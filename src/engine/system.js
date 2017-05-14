8c141f88060eccb3eb1ab21b34a393a7c6be8620
cyberblast <git@cyberblast.org>
2017-05-14 14:45:07 +0200

    fixing merge errors

--- src/engine/system.js
+++ src/engine/system.js
@@ -25,7 +25,7 @@ function Feature(name, setting){
     this.files = {};
     this.requiresMemory = false;
     this.memory = null;
-    this.logScopes = null;  
+    this.logScopes = null;
     // additional partitions
     this.memoryPartitions = [];
 
@@ -292,6 +292,7 @@ const system = {
             features.forEach(this.installFeature);
             global.installedVersion = mod.DEPLOYMENT;
         }
+        
         // setup memory
         memory.init();
         _.invoke(global.feature, 'initMemory');
